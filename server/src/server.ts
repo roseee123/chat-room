02c137637bf6c7567afec83d8a0b9f54fe710771
DESKTOP-7PV5CTF\yiyun <rose52924@gmail.com>
2021-11-28 17:18:55 +0800

    後台將訊息傳到資料庫

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,9 @@
 import * as express from 'express';
 import * as http from "http";
 import * as socketio from "socket.io";
+import { Message } from "./message";
+import { Schema, model,  connect } from 'mongoose';
+const DB_CONNECT = 'mongodb://localhost:27017/chat-room'';
 
 const app = express();
 const httpServer = http.createServer();
@@ -10,6 +13,15 @@ const io = new socketio.Server(httpServer, {
     }
 });
 
+const messagesSchema = new Schema<Message>(
+    {
+        User: String,
+        Content: String,
+        SendTime: Date
+    }
+);
+const MessageModel = model<Message>('Message',messagesSchema);
+
 app.get('/', (request, response) => {
     response.send('Hello world!');
 });
@@ -17,16 +29,21 @@ app.get('/', (request, response) => {
 io.on('connection', (socket) => {
     console.log('a user connected');
 
-    io.emit("message", 'Hello wWrld!');
+    //io.emit("message", 'Hello wWrld!');
 
     socket.on("disconnect", () => {
         console.log("a user go out");
     });
 
-    socket.on("sendMessage", (obj) => {
-        io.emit("message", obj);
+    socket.on("sendMessage", (m: Message) => {
+        console.log("[server](message): %s", JSON.stringify(m));
+        io.emit("message", m);
+        const newMessage = new MessageModel(m);
+        newMessage.save();
     });
 
 });
+
+connect(DB_CONNECT);
 
 httpServer.listen(8080);
